68523e7b3aad1095b59fb56d99d19af1a998aa98
Jonathan Herrera <jonathanh1234@gmail.com>
2025-02-05 18:36:51 -0600

    index

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ValentineButtonsComponent } from './valentine-buttons/valentine-buttons.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: ValentineButtonsComponent },
+  // Add other routes here
+  { path: '**', redirectTo: '', pathMatch: 'full' } // Redirect to home for unknown routes
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes, { useHash: false })], // Set useHash to true if needed
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
